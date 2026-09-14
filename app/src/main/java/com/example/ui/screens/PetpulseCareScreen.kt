c0100d285483480a88b1775ca49f1a9b52897ee0
jithurenjith1 <jithurenjith1@gmail.com>
2026-09-14 10:24:40 +0530

    fix: FabPosition import in PetpulseCareScreen

--- app/src/main/java/com/example/ui/screens/PetpulseCareScreen.kt
+++ app/src/main/java/com/example/ui/screens/PetpulseCareScreen.kt
@@ -8,7 +8,7 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.FabPosition
+import androidx.compose.material3.FabPosition
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -586,4 +586,5 @@ private fun WhatsIncludedSection(palette: PetpulsePalette) {
         }
     }
 }
+
 
